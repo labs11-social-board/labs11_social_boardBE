205f4a3a2424f8d7cdb710aefa6d1cff0dfa474d
Huthman King <kinghuthman@gmail.com>
2019-03-20 13:57:06 -0400

    Test

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require('./server.js');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 server.listen(port, () =>
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
 );
