f14d9273e090dba70264451f8f76866e9a66d0e0
Brandon White <bwhite06@stac.edu>
2019-04-03 10:39:07 -0400

    Update userModeratorDB.js
    added check to and cleaned up promise

--- backend/db/models/userModeratorDB.js
+++ backend/db/models/userModeratorDB.js
@@ -8,32 +8,61 @@ const getModerators = (id) => {
         .join('user_settings as us', 'us.user_id', 'users.id')
         .orderBy('us.user_permissions', 'desc')
 };
-
+// change user to moderator 
 const changeToModerator = (changeUser_id) => {
-    let v = db('user_settings')
-    .where({'user_id': changeUser_id})
-    .select('user_permissions')
-if(v.user_permissions != 'moderator'){
+
+  return db('user_settings')
+ .where({'user_id': changeUser_id})
+ .select('user_permissions')
+ .then(data=>{
+  console.log(data[0].user_permissions);
+  if(data[0].user_permissions != 'moderator'){
+    
+       return db('user_settings')
+        .where({'user_id': changeUser_id})
+        .update({'user_permissions': 'moderator'})
+
+ }
+ else{
+    console.log('user is already moderator');
+    return getModerators(changeUser_id);
+}
+
+}).catch(err=>{
+  return err;
+    
+});
+        
+    
+
+
+
+};
+
+
+const changeToBasic = (changeUser_id) => {
+ 
     return db('user_settings')
     .where({'user_id': changeUser_id})
-    .update({'user_permissions': 'moderator'})
-
-}
-    
-
-    
-};
-const changeToBasic = (changeUser_id) => {
-    let v = db('user_settings')
-    .where({'user_id': changeUser_id})
     .select('user_permissions')
-    if(v.user_permissions = 'moderator'){
-        return db('user_settings')
-        .where({'user_id': changeUser_id})
-        .update({'user_permissions': 'basic'})
+    .then(data=>{
+     console.log(data[0].user_permissions);
+     if(data[0].user_permissions != 'basic'){
+       
+          return db('user_settings')
+           .where({'user_id': changeUser_id})
+           .update({'user_permissions': 'basic'})
+   
     }
-    
-    
+    else{
+       console.log('user is not moderator already');
+       return getModerators(changeUser_id);
+   }
+   
+   }).catch(err=>{
+     return err;
+       
+   });
 
     
 };
