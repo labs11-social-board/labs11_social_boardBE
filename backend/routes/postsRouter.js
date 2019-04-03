b19b8304b07e6270e2dc0c402bf25709d9789c68
jeff15113 <itsmejeffery@gmail.com>
2019-04-03 16:51:56 -0400

    modified delete post route

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -3,7 +3,11 @@
  **************************************************************************************************/
 require('dotenv').config();
 const express = require('express');
-const { postsDB, discussionFollowsDB, userNotificationsDB } = require('../db/models/index.js');
+const {
+  postsDB,
+  discussionFollowsDB,
+  userNotificationsDB
+} = require('../db/models/index.js');
 const router = express.Router();
 
 const { maxNumOfNotifications } = require('../config/globals.js');
@@ -25,9 +29,12 @@ router.get('/search', (req, res) => {
   if (order === 'undefined') order = null;
   if (orderType === 'undefined') orderType = null;
   if (!searchText) return res.status(200).json([]);
-  return postsDB.search(searchText, order, orderType)
+  return postsDB
+    .search(searchText, order, orderType)
     .then(results => res.status(200).json(results))
-    .catch(err => res.status(500).json({ error: `Failed to search(): ${err}` }));
+    .catch(err =>
+      res.status(500).json({ error: `Failed to search(): ${err}` })
+    );
 });
 
 // create a post by a given user_id to a given discussion_id
@@ -35,29 +42,35 @@ router.post('/:user_id', authenticate, (req, res) => {
   const { user_id } = req.params;
   const { discussion_id, postBody, repliedPostID } = req.body;
   const created_at = Date.now();
-  if (!postBody) return res.status(400).json({ error: 'Post body must not be empty.' });
+  if (!postBody)
+    return res.status(400).json({ error: 'Post body must not be empty.' });
   const newPost = { user_id, discussion_id, body: postBody, created_at };
   if (repliedPostID) newPost.reply_to = repliedPostID;
   return postsDB
     .insert(newPost)
     .then(async newId => {
-      const discFollowers = await discussionFollowsDB.getFollowers(discussion_id);
+      const discFollowers = await discussionFollowsDB.getFollowers(
+        discussion_id
+      );
       discFollowers.forEach(async user => {
-        const newNotification = { user_id: user.user_id, discussion_id, post_id: newId[0], created_at };
+        const newNotification = {
+          user_id: user.user_id,
+          discussion_id,
+          post_id: newId[0],
+          created_at
+        };
         const notifications = await userNotificationsDB.getCount(user.user_id);
         if (parseInt(notifications.count) >= maxNumOfNotifications) {
           await userNotificationsDB.removeOldest(user.user_id);
         }
         await userNotificationsDB.add(newNotification);
-        pusher.trigger(
-          `user-${ user.uuid }`,
-          'notification',
-          null,
-        );
+        pusher.trigger(`user-${user.uuid}`, 'notification', null);
       });
-      return res.status(201).json({ message: 'Post creation successful.' })
+      return res.status(201).json({ message: 'Post creation successful.' });
     })
-    .catch(err => res.status(500).json({ error: `Failed to insert(): ${err}` }));
+    .catch(err =>
+      res.status(500).json({ error: `Failed to insert(): ${err}` })
+    );
 });
 
 // edit post with given post id
@@ -65,22 +78,27 @@ router.put('/:user_id', authenticate, (req, res) => {
   const { post_id, postBody } = req.body;
   const last_edited_at = Date.now();
   const post = { body: postBody, last_edited_at };
-  if (!postBody) return res.status(400).json({ error: 'Post body must not be empty.' });
+  if (!postBody)
+    return res.status(400).json({ error: 'Post body must not be empty.' });
   if (!post_id) return res.status(400).json({ error: 'Post ID is required.' });
   return postsDB
     .update(post_id, post)
     .then(() => res.status(201).json({ message: 'Post update successful.' }))
-    .catch(err => res.status(500).json({ error: `Failed to update(): ${err}` }));
+    .catch(err =>
+      res.status(500).json({ error: `Failed to update(): ${err}` })
+    );
 });
 
 // remove post with given post id
-router.delete('/:user_id', authenticate, (req, res) => {
-  const post_id = req.get('post_id');
+router.delete('/:user_id/:post_id', authenticate, (req, res) => {
+  const { post_id } = req.params;
   if (!post_id) return res.status(400).json({ error: 'Post ID is required.' });
   return postsDB
     .remove(post_id)
     .then(() => res.status(201).json({ message: 'Post removal successful.' }))
-    .catch(err => res.status(500).json({ error: `Failed to remove(): ${err}` }));
+    .catch(err =>
+      res.status(500).json({ error: `Failed to remove(): ${err}` })
+    );
 });
 
 module.exports = router;
