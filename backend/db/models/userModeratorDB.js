8532d58e662613cf75994e8801329506a87496c1
Brandon White <bwhite06@stac.edu>
2019-04-02 21:10:56 -0400

    toggle for moderator
    ---changed usermoderator

--- backend/db/models/userModeratorDB.js
+++ backend/db/models/userModeratorDB.js
@@ -1,14 +1,53 @@
 const db = require('../dbConfig.js');
 
 // GET ALL USERS THAT ARE MODERATORS
-const getModerators = () => {
+const getModerators = (id) => {
     return db('users')
-        .where('us.user_permissions', 'moderator')
+       .where({'id': id})
         .select('id', 'username', 'email', 'status', 'us.user_permissions')
         .join('user_settings as us', 'us.user_id', 'users.id')
         .orderBy('us.user_permissions', 'desc')
 };
 
+const changeToModerator = (changeUser_id) => {
+    let v = db('user_settings')
+    .where({'user_id': changeUser_id})
+    .select('user_permissions')
+if(v.user_permissions != 'moderator'){
+    return db('user_settings')
+    .where({'user_id': changeUser_id})
+    .update({'user_permissions': 'moderator'})
+
+}
+    
+
+    
+};
+const changeToBasic = (changeUser_id) => {
+    let v = db('user_settings')
+    .where({'user_id': changeUser_id})
+    .select('user_permissions')
+    if(v.user_permissions = 'moderator'){
+        return db('user_settings')
+        .where({'user_id': changeUser_id})
+        .update({'user_permissions': 'basic'})
+    }
+    
+    
+
+    
+};
+
+ //const userModerator = (changeUser_id) => {
+  //  return db('users').where({'id': changeUser_id})
+  //  .select('id', 'username', 'email', 'status', 'us.user_permissions')
+ //   .join('user_settings as us', 'us.user_id', 'users.id')
+ //   .orderBy('us.user_permissions', 'desc')
+ //};
+
+
 module.exports = {
-    getModerators
+    getModerators,
+    changeToModerator,
+    changeToBasic
 }
--- backend/routes/userModeratorRouter.js
+++ backend/routes/userModeratorRouter.js
@@ -3,9 +3,9 @@ const express = require('express');
 const userModeratorDB = require('../db/models/userModeratorDB');
 const router = express.Router();
 
-router.get('/mod', (req, res) => {
+router.get('/:id', (req, res) => {
     return userModeratorDB
-        .getModerators()
+        .getModerators(req.params.id)
         .then(moderators => {
             res.status(200).json(moderators)
         })
@@ -15,5 +15,36 @@ router.get('/mod', (req, res) => {
             })
         })
 })
+router.get('/changeToMod/:user_id', (req, res) => {
 
+    return userModeratorDB
+        .changeToModerator(req.params.user_id)
+        .then(moderators => {
+            if(moderators===1){
+                res.status(202).json(moderators)
+            }
+            console.log(moderators)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: `Failed to update${err}`
+            })
+        })
+})
+router.get('/changeToBasic/:user_id', (req, res) => {
+
+    return userModeratorDB
+        .changeToBasic(req.params.user_id)
+        .then(moderators => {
+            if(moderators===1){
+                res.status(202).json(moderators)
+            }
+            console.log(moderators)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: `Failed to update${err}`
+            })
+        })
+})
 module.exports = router;
