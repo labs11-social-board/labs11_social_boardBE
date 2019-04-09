ee50f796093597c424a84a8931c9fd386af747a1
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-09 15:48:56 -0500

    spacing fix

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -609,7 +609,6 @@ router.put('/last-login/:user_id', authenticate, (req, res) => {
     );
 });
 
-
 // Delete a user by their ID
 router.delete('/:user_id', authenticate, (req, res) => {
   const { user_id } = req.params;
