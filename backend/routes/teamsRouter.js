6d9e0b8f988edb966e26ad3e95cf1e70dd4f50a7
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-09 15:10:50 -0500

    Updated the update teams route to add the new team logo if they did not already have one

--- backend/routes/teamsRouter.js
+++ backend/routes/teamsRouter.js
@@ -88,15 +88,22 @@ router.put('/:user_id/:team_id', authenticate, checkRole, async (req, res) => {
     const updated = await teamsDB.updateTeamBoard(team_id, changes);
 
     if(image){
-      const updated = await teamsDB.updateTeamLogo(team_id, image);
-    }
+      const isImageAlready = await teamsDB.checkIfTeamHasImage(team_id);
+      if(!isImageAlready) {
+        const newImage = await teamsDB.updateImageWithTeam(image.id, team_id);
 
-    if (updated === null) {
-      res.status(400).json({
-        error: 'Only the Team Owner can update the Teams information'
-      });
-    } else {
-      res.status(200).json(updated);
+        res.status(200).json(newImage);
+      } else {
+        const updated = await teamsDB.updateTeamLogo(team_id, image.image);
+
+        if (updated === null) {
+          res.status(400).json({
+            error: 'Only the Team Owner can update the Teams information'
+          });
+        } else {
+          res.status(200).json(updated);
+        }
+      }
     }
   } catch (err) {
     res
