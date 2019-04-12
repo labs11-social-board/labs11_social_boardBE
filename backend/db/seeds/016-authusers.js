e063bc1ed273d535762bff1fc351f585d58d9447
amberm <nekosenpai@gmail.com>
2019-04-12 10:12:59 -0600

    updated seed Auth Users for front end compliance

--- backend/db/seeds/016-authusers.js
+++ backend/db/seeds/016-authusers.js
@@ -5,12 +5,15 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('approved_emails').insert([
-        {id: 1, email: 'amber@amberprograms.com', first_name: 'amber', last_name: 'meador', created_at: new Date.now()},
-        {id: 2, email: 'huff@example.com', first_name: 'huff', last_name: 'king', created_at: new Date.now()},
-        {id: 3, email: 'imonovbude@gmail.com', first_name: 'imon', last_name: 'imon', created_at: new Date.now()},
-        {id: 4, email: 'trai@example.com', first_name: 'trai', last_name: 'trai', created_at: new Date.now()},
-        {id: 5, email: 'modesto@example.com', first_name: 'modesto', last_name: 'modesto', created_at: new Date.now()},
-        {id: 6, email: 'jonathan@example.com', first_name: 'jonathan', last_name: 'jonathan', created_at: new Date.now()},
+        {id: 1, email: 'amber@amberprograms.com', first_name: 'amber', last_name: 'meador', },
+        {id: 2, email: 'huff@example.com', first_name: 'huff', last_name: 'king', },
+        {id: 3, email: 'imonovbude@gmail.com', first_name: 'imon', last_name: 'imon', },
+        {id: 4, email: 'trai@example.com', first_name: 'trai', last_name: 'trai', },
+        {id: 5, email: 'modesto@example.com', first_name: 'modesto', last_name: 'modesto', },
+        {id: 6, email: 'jonathan@example.com', first_name: 'jonathan', last_name: 'jonathan', },
+        {id: 7, email: 'marcoguzman16@gmail.com', first_name: 'marco', last_name: 'guzman', },
+        {id: 8, email: 'itsmejeffery@gmail.com', first_name: 'jeffery', last_name: 'jeffery', },
+        {id: 9, email: 'kevintrandeveloper@gmail.com', first_name: 'kevin', last_name: 'jeffery', },
         
       ]);
     });
