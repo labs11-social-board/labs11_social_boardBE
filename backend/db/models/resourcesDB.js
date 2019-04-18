31ae77bad59eca2849fba19efb52623b2c364644
Imon Ovbude <imonovbude@gmail.com>
2019-04-18 14:00:16 -0700

    modified resourcesDB

--- backend/db/models/resourcesDB.js
+++ backend/db/models/resourcesDB.js
@@ -1,17 +1,17 @@
 const db = require('../dbConfig.js');
 
 // GET ALL RESOURCES AND USERS WHO CREATED THEM
-async function getResources () {
+async function getResources() {
     const query = await db('resources as r')
-    .select('r.title', 'r.resource', 'r.info', 'u.username')
-    .join('users as u', 'u.id', 'r.user_id')
+        .select('r.title', 'r.resource', 'r.info', 'u.username')
+        .join('users as u', 'u.id', 'r.user_id')
 
     return query;
 }
 
-const insertResource = (user_id, resource, info, title) => {
+const insertResource = (user_id, resource, title, info) => {
     return db('resources')
-    .insert({'title': title, 'resource': resource, 'info': info, 'user_id': user_id})
+        .insert({ 'title': title, 'resource': resource, 'info': info, 'user_id': user_id })
 }
 
 module.exports = {
