afb1292d0a0eca6654d0d46c5a4af5d25649409c
Imon Ovbude <imonovbude@gmail.com>
2019-04-19 13:19:48 -0700

    fixed bug on delete

--- backend/db/migrations/20190417082758_create_resources_table.js
+++ backend/db/migrations/20190417082758_create_resources_table.js
@@ -7,18 +7,15 @@ exports.up = function (knex, Promise) {
         tbl
             .text('title', 500)
             .notNullable()
-            .onDelete('CASCADE')
 
         tbl
             .text('resource', 2048)
             .notNullable()
             .unique()
-            .onDelete('CASCADE')
 
         tbl
             .text('info', 2048)
             .notNullable()
-            .onDelete('CASCADE')
 
         tbl
             .integer('user_id')
